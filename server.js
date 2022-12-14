33d531cd0227245af0d8ef5cc42a2a2069bdf8f9
AegisVP <vlad@pysarenko.com>
2022-12-14 23:23:47 +0100

    install swagger and mongoose

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+const router = require("express").Router();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerDocument));
