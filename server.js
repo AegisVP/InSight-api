ee9d61bb11fb1476427ccaf9007b79a4ac11a976
Mykhailo Shyhin <johnglagol@gmail.com>
2022-12-15 10:48:57 +0200

    add conection db and start server
    without swagger yet

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-const router = require("express").Router();
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+require('dotenv').config();
+const app = require('./app');
+const { mongoConnect } = require('./db/connections');
 
-router.use("/api-docs", swaggerUi.serve);
-router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+const PORT = process.env.PORT || 3000;
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+const start = async () => {
+  try {
+    await mongoConnect();
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
