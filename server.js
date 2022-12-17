ec6f6d2412c1a8d404c6f1801c8a5f2a160d2cd4
AegisVP <vlad@pysarenko.com>
2022-12-17 06:26:51 +0100

    swagger variant by Vlad

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-const router = require("express").Router();
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8088;
 
-router.use("/api-docs", swaggerUi.serve);
-router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger_vlad.json");
+
+app.use(
+	"/api-docs-vlad",
+	swaggerUi.serveFiles(swaggerDocument, {
+		swaggerOptions: {
+			url: "/api-docs-vlad/swagger.json",
+		},
+	}),
+	swaggerUi.setup(swaggerDocument)
+);
+
+try {
+	app.listen(PORT, () => {
+		console.log(`Server running. Use our API on port: ${PORT}`);
+	});
+} catch (error) {
+	console.error("Error:", error.message);
+	process.exit(1);
+}
