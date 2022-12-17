6d8d1e8e683ce05dd14f47bf6438dae20cbf6f27
DaryaGrygorova <grygorova.darya@gmail.com>
2022-12-17 10:26:33 +0200

    add server config

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-const router = require("express").Router();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8088;
+
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
-router.use("/api-docs", swaggerUi.serve);
-router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+app.use(
+  "/api-docs",
+  swaggerUi.serveFiles(swaggerDocument, {
+    swaggerOptions: {
+      url: "/api-docs/swagger.json",
+    },
+  }),
+  swaggerUi.setup(swaggerDocument)
+);
+
+try {
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+} catch (error) {
+  console.error("Error:", error.message);
+  process.exit(1);
+}
