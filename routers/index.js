df467cded9ca14b56eafcf1d3a4e671ba4153b01
AegisVP <vlad@pysarenko.com>
2022-12-19 19:43:41 +0100

    merge fix

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,5 @@
-const authRouter = require('./authRouter');
 const userRouter = require('./userRouter');
 const frontendRouter = require('./frontendRouter');
+const diaryRouter = require('./diaryRouter');
 
-module.exports = { authRouter, userRouter, frontendRouter };
+module.exports = { frontendRouter, userRouter, diaryRouter };
