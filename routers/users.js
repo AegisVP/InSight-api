ffa74fd15b4d0e3217d662edd93fceae7ea276fb
Mykhailo Shyhin <johnglagol@gmail.com>
2022-12-19 16:30:39 +0200

    add endpoint current
    Додав Ендпоінт Current, змінив ендпоінт на реєстрацію та переробив відповіді сервера, щоб співпадало зі сваггером

--- routers/users.js
+++ routers/users.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const { signupController, loginController, logoutController } = require('../controllers/userController');
+const {
+  signupController,
+  loginController,
+  logoutController,
+  currentUserController,
+} = require('../controllers/userController');
 const tryCatchWrapper = require('../utils/tryCatchWrapper');
 const { loginValidation, signupValidation } = require('../middlewares/validationMiddleware');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/signup', signupValidation, tryCatchWrapper(signupController));
+router.post('/', signupValidation, tryCatchWrapper(signupController));
 
 router.post('/login', loginValidation, tryCatchWrapper(loginController));
 
-router.post('/logout', authMiddleware, tryCatchWrapper(logoutController));
+router.patch('/logout', authMiddleware, tryCatchWrapper(logoutController));
+
+router.get('/current', authMiddleware, tryCatchWrapper(currentUserController));
 
 module.exports = router;
