373809cf3ef5da4d66bebfb2614818f384fd289b
DaryaGrygorova <grygorova.darya@gmail.com>
2022-12-19 20:49:28 +0200

    Refactoring diary controllers

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const authRouter = require('./authRouter');
 const userRouter = require('./userRouter');
 const frontendRouter = require('./frontendRouter');
+const diaryRouter = require('./diaryRouter');
 
-module.exports = { authRouter, userRouter, frontendRouter };
+module.exports = { authRouter, userRouter, frontendRouter, diaryRouter };
