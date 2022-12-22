77ba4b7d77caca70a2c2992338264951e8db4dec
AegisVP <vlad@pysarenko.com>
2022-12-23 00:41:05 +0100

    prettier, asign default in config

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ async function start() {
   try {
     mongoConnect();
 
-    app.listen(PORT  || 8080, () => {
-      console.log(`Server running. Use our API on port: ${PORT || 8080}`);
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   } catch (error) {
     console.error('Error:', error.message);
