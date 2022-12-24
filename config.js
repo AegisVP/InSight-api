6f4c6962f81240b74a2db3a0690d9d38576d944b
AegisVP <vlad@pysarenko.com>
2022-12-24 03:25:15 +0100

    ability to disable local frontend

--- config.js
+++ config.js
@@ -5,8 +5,9 @@ module.exports = {
   PORT: process.env.PORT || 8088,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_FRONTEND_HANDLER: process.env.GOOGLE_FRONTEND_HANDLER,
   BACKEND_URL: process.env.BACKEND_URL,
   FRONTEND_URL: process.env.FRONTEND_URL,
-  GOOGLE_FRONTEND_HANDLER: process.env.GOOGLE_FRONTEND_HANDLER,
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'JWT Secret Message',
+  FRONTEND_LOCAL: Boolean(process.env.FRONTEND_LOCAL),
 };
--- routers/frontendRouter.js
+++ routers/frontendRouter.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const { FRONTEND_LOCAL, FRONTEND_URL } = require('./config');
 
-router.use(express.static(path.resolve('./public')));
+if (FRONTEND_LOCAL) {
+  router.use(express.static(path.resolve('./public')));
 
-router.get('*', (_, res) => {
-  return res.sendFile(path.resolve('./public/index.html'));
-});
+  router.get('*', (_, res) => {
+    return res.sendFile(path.resolve('./public/index.html'));
+  });
+
+} else {
+  router.get((_, res) => {
+    return res.redirect(FRONTEND_URL);
+  });
+}
 
 module.exports = router;
